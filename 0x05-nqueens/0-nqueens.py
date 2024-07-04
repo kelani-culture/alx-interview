b138f55ba49877f9d1be9e4192cc8bae877da8d6
Null <kelanidarasimi9@gmail.com>
2024-07-04 20:32:38 +0100

    Nqueens algorithm

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,77 +1,83 @@
-#!/usr/bin/python3
-"""this module attempts to solve the nqueens problem"""
-
+#!/usr/bin/python
 import sys
+"""Nqueens alogrinthm solved"""
 
+class NQueens:
+    """N queens object solved"""
+    def __init__(self, n):
+        self.n = n
+        self.board = [[0 for _ in range(n)] for _ in range(n)]
+        self.solutions = []
 
-class Nqueens:
-    """a class that has the method and util
-        functions to solve the nqueens problem"""
+    def is_safe(self, row, col):
+        """ check is safe method"""
+        # Check this row on the left side
+        for i in range(col):
+            if self.board[row][i] == 1:
+                return False
 
-    def __init__(self, nums):
-        """the entry point into the class instance"""
-        self.nums = nums
-        self.board = []
+        # Check upper diagonal on the left side
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if self.board[i][j] == 1:
+                return False
+
+        # Check lower diagonal on the left side
+        for i, j in zip(range(row, self.n, 1), range(col, -1, -1)):
+            if self.board[i][j] == 1:
+                return False
+
+        return True
+
+    def solve_nqueens_util(self, col):
+        """ solve nqueens utils"""
+        if col >= self.n:
+            self.solutions.append([list(row) for row in self.board])
+            return True
+
+        res = False
+        for i in range(self.n):
+            if self.is_safe(i, col):
+                self.board[i][col] = 1
+                res = self.solve_nqueens_util(col + 1) or res
+                self.board[i][col] = 0
+
+        return res
 
     def solve(self):
-        """this utility performs validation on the command line argument N
-            before attempting to solve the N-queens problem"""
-        self.cols = set()
-        self.rows = set()
-        self.pos_diag = set()
-        self.neg_diag = set()
-        self.res = []
-        self.hash = dict()
+        """ solve nqueens method"""
+        self.solve_nqueens_util(0)
+        return self.solutions
 
-        if (type(self.nums) is not int):
-            try:
-                self.nums = int(self.nums)
-            except Exception:
-                print("N must be a number")
-                return
-        if (self.nums < 4):
-            print("N must be at least 4")
-            return
-        self.backtrack(0)
-        return self.res
-
-    def backtrack(self, r):
-        """the main backtracking algorithm used in the n-queens solution"""
-        if (r == self.nums):
-            self.res.append(list(self.hash.values()))
-            self.hash = dict()
-            return
-
-        for c in range(self.nums):
-            if (c in self.cols or (r - c) in self.neg_diag or
-                    (r + c) in self.pos_diag):
-                continue
-            self.cols.add(c)
-            self.neg_diag.add(r - c)
-            self.pos_diag.add(r + c)
-            self.hash[f"{r}{c}"] = [r, c]
-            self.board.append(self.hash[f"{r}{c}"])
-
-            self.backtrack(r + 1)
-
-            self.cols.remove(c)
-            self.neg_diag.remove(r - c)
-            self.pos_diag.remove(r + c)
-            if (f"{r}{c}" in self.hash):
-                self.hash.pop(f"{r}{c}")
+    def print_solutions(self):
+        """ print nqueens solutions"""
+        for solution in self.solutions:
+            result = []
+            for i in range(self.n):
+                for j in range(self.n):
+                    if solution[i][j] == 1:
+                        result.append([i, j])
+            print(result)
 
 
 def main():
-    """the entry point into the program"""
-    if __name__ != "__main__":
-        return
-    if (len(sys.argv) < 2):
-        print("no arguments provided!")
-        return
-    input = sys.argv[1]
-    queens = Nqueens(input)
-    solution = queens.solve()
-    if (not solution):
-        return
-    for i in solution:
-        print(i)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    nqueens = NQueens(n)
+    nqueens.solve()
+    nqueens.print_solutions()
+
+
+if __name__ == "__main__":
+    main()
